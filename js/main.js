f1c21dd2050bfe5344016bf7e5f672944c2ac755
Ivas Alencar <alencarfilho22@gmail.com>
2024-06-04 10:09:39 -0300

    Adicionando evento de subtração e adição no braço

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", (evento) => {
-    console.log(evento);
-});
 
-function dizOi (nome) {
+somar.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) +1;
+})
 
-    console.log("Olá," + nome);
-    console.log("Bem vindo ao Jogo Robotron 2000")
-}
-
-dizOi('Ivas');
+subtrair.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) -1;
+})
