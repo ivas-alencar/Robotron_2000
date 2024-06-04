a1d100d4b6af0d0937dcf45496795bf7c233621c
Ivas Alencar <alencarfilho22@gmail.com>
2024-06-04 10:24:58 -0300

    Tornando o códig do braço menos repetitivo

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) +1;
-})
+somar.addEventListener("click", () => {manipulaDados("somar")});
+subtrair.addEventListener("click", () => {manipulaDados("subtrair")});
 
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) -1;
-})
+function manipulaDados(operacao) {
+    if (operacao === "subtrair") {
+        braco.value = parseInt(braco.value) -1;
+    } else {
+        braco.value = parseInt(braco.value) +1;
+    }
+}
